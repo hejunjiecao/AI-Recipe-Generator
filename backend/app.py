86d34026de5ed8a709e89a1c9ea037666339eb7e
Sangyao42 <106156781+Sangyao42@users.noreply.github.com>
2024-06-18 03:19:08 +0200

    fix(python dependencies)

--- backend/app.py
+++ backend/app.py
@@ -2,28 +2,36 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 
+import setting
+import gpt
+
 app = Flask(__name__)
 CORS(app)
 
 UPLOAD_FOLDER = 'uploads'
+save_path = ''
 
 @app.route("/upload", methods=["POST"])
 def upload_image():
     if 'file' not in request.files:
         return jsonify({"error": "No file part"})
-    
+
     file = request.files['file']
-    
+
     if file.filename == '':
         return jsonify({"error": "No selected file"})
-    
+
     # 将文件保存到本地
     save_path = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(save_path)
-    
+
     # 打印保存路径
     print(f"File saved to: {save_path}")
-    
+    myGPT = MyGPT(configs)
+    user_msgs = generate_user_msg(save_path)
+    response = myGPT.query(user_msgs)
+
+    # return jsonify({"message": response['choices'][0]['message']['content']})
     return jsonify({"message": "File successfully uploaded", "filename": file.filename})
 
 @app.route('/uploads/<filename>')
--- backend/setting.py
+++ backend/setting.py
@@ -1,5 +1,5 @@
 import yaml
-# import requests
+import requests
 import base64
 from pathlib import Path
 import os
