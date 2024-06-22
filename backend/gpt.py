025d9a3c8dcbf849a9c2cbe94c5fbed8069838dd
Sangyao42 <106156781+Sangyao42@users.noreply.github.com>
2024-06-23 00:29:41 +0200

    feat(recognize): use few-shot to recognize ingredients in the uploaded image. Response is a json format with ingredients names as keys and quantity as values. Untested

--- backend/gpt.py
+++ backend/gpt.py
@@ -31,6 +31,8 @@ class MyGPT:
           }
       ]
       print(img_paths)
+      if img_paths == None:
+         return content
       for img_path in img_paths:
         # encode the image
         b64_img = self.encode_image(img_path)
@@ -47,8 +49,8 @@ class MyGPT:
       print(self.model)
       encoded_msgs = []
       for msg in msgs:
-        if msg.role == 'user':
-            content = self.compose_content(msg.prompt, msg.imgs)
+        # if (msg.imgs) != None:
+        content = self.compose_content(msg.prompt, msg.imgs)
         encoded_msgs.append({
             "role": msg.role,
             "content": content
@@ -68,19 +70,50 @@ class MyGPT:
       response = setting.requests.post(self.api_base, headers=headers, json=payload).json()
       return response
 
+def process_response(response, GPTModel):
+  # Intended behaviour of this function:
+  # returns True, "textual reply from GPT", and prints the cost at the same time
+  # OR returns False, "reason why the error occurs"
+  print("response: ")
+  print(response)
+  if "error" not in response:
+    usage = response["usage"]
+    prompt_tokens = usage["prompt_tokens"]
+    completion_tokens = usage["completion_tokens"]
+
+    if GPTModel == 'gpt-3.5-turbo':
+      prompt_cost = prompt_tokens / 1000000 * 0.5
+      response_cost = completion_tokens / 1000000 * 1.5
+      print(f"This round costs ${'{0:.5f}'.format(prompt_cost + response_cost)}")
+
+    elif GPTModel == 'gpt-4-turbo':
+      prompt_cost = prompt_tokens / 1000000 * 10
+      response_cost = completion_tokens / 1000000 * 30
+      print(f"This round costs ${'{0:.5f}'.format(prompt_cost + response_cost)}")
+
+    elif GPTModel == 'gpt-4o':
+      prompt_cost = prompt_tokens / 1000000 * 5
+      response_cost = completion_tokens / 1000000 * 15
+      print(f"This round costs ${'{0:.5f}'.format(prompt_cost + response_cost)}")
+    else:
+      return False, response["error"]["message"]
+
+    return True, response['choices'][0]['message']['content']
+  else:
+    return False, response["error"]["message"]
+
 if __name__ == '__main__':
     print(setting.config_path)
     for key, value in setting.configs.items():
         print(key, ':', value)
     myGPT = MyGPT(setting.configs)
-    user_msg = GPTMsg('user', 'Tell me how good is restaurant shown in the image', [setting.Path(setting.root_dir) / 'uploads' /'test_image.jpg'])
+    user_msg = GPTMsg('user', 'Generate a recipe based on the ingredients on the image', [setting.Path(setting.root_dir) / 'uploads' /'test_image.jpg'])
     msgs = []
     msgs.append(user_msg)
 
-    response = myGPT.query(msgs)
-    # print(response['choices'][0]['message']['content'])
-    print(response)
-    # result, reply = process_response(...)
-    # if not result:
-    #   print("Error! See the deatils below.")
+    result, reply = process_response(myGPT.query(msgs), myGPT.model)
+    if not result:
+      print("Error! See the deatils below.")
+    print(reply)
+    # reply = myGPT.query(msgs)
     # print(reply)
